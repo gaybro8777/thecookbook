4da7eba9e4e984f505b3aa959c94d2ed67b6c166
Matt Galvin <mgalvin@endpoint.com>
2018-01-27 19:24:44 +0000

    Order fork history recipes by id

--- app/models/fork_history.rb
+++ app/models/fork_history.rb
@@ -2,6 +2,6 @@ class ForkHistory < ActiveRecord::Base
   belongs_to :recipe, foreign_key: :id
 
   def recipe_chain
-    Recipe.find(self.path)
+    Recipe.where(id: self.path).order('id ASC')
   end
 end
